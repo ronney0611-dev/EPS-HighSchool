81d76953bec04d26fcf7b9a5a5961b687dadb80e
Ronney0611 <ronney0611@gmail.com>
2026-04-17 04:44:08 +0100

    ts fixed

--- hooks/useClasses.ts
+++ hooks/useClasses.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 
+type Class = { name: string, students: string[] }
 export const useClasses = () => {
-  const [classes, setClasses] = useState(() => {
+  const [classes, setClasses] = useState<Class[]>(() => {
   if (typeof window === 'undefined') return []
   const saved = localStorage.getItem('classes')
   return saved ? JSON.parse(saved) : []
